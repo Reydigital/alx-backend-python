ab28c14195fe3bd0fb631bed4391c29e52e5163b
Reydigital <regysmarie@gmail.com>
2023-01-06 01:09:00 +0000

    commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+
+This module contains a type-annotated function.
+
+"""
+
+
+
+
+
+def floor(n: float) -> int:
+
+        """
+
+            Args:
+
+                    n: The float.
+
+                        Returns:
+
+                                The floor of the float.
+
+                                    """
+
+                                        return int(n)
